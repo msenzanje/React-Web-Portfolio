686648363002d1fb6bc2511c0d2cbf07b11d9bff
msenzanje <ksenzenaje@gmail.com>
2024-08-12 21:53:38 -0500

    Intro Section
    - Replaced placeholder text
    
    - Replaced placeholder portrait image

--- react-portfolio/src/components/Navbar/Intro/Intro.jsx
+++ react-portfolio/src/components/Navbar/Intro/Intro.jsx
@@ -6,17 +6,19 @@ import { getImageUrl } from "../../../utils";
 export const Intro = () => {
     return (<section className={styles.container}>
         <div className= {styles.content}>
-            <h1 className= {styles.title}>Test Text</h1>
+            <h1 className= {styles.title}>Hi, I'm Melusi</h1>
             <p className={styles.description}>
-                Blah Blah Blah Blah Blah Blah 
-                Blah Blah BlahBlah Blah Blah
+                I am a Computer Science student set to
+                graduate in 2026. I have expeience Java, C, 
+                and the Flutter framework. <br/>
+                Reach out if you'd like to learn more!
             </p>
             <a href="mailto:email@email.com" className={styles.contactBtn}>
                 Contact Me
             </a>
         </div>
         <img 
-            src={getImageUrl("hero/heroImage.png")} 
+            src={getImageUrl("hero/portraitIcon.png")} 
             alt="Potrait photo of me" 
             className={styles.heroImg}
         />
