969e654701dfd7763b8eb5633ea57fac422fc450
msenzanje <ksenzanje@gmail.com>
2024-12-22 20:35:16 -0600

    Projects section Updates
    -Initiated Viz projectCard, addding a description and software used.
    
    -Minor styling fixes

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -7,7 +7,7 @@ import styles from "./Projects.module.css"
 export const Projects = () => {
     return <section className={styles.container} id="projects">
         <h2 className={styles.title}>Projects</h2>
-        <div className={styles.project}>{
+        <div className={styles.projects}>{
             projects.map((project, id) => {
                 return (
                     <ProjectCard key={id} project={project}/>
