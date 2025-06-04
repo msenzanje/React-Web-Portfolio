57fe30fe1a7072c036ec472a47c56921cda5fafc
msenzanje <ksenzanje@gmail.com>
2025-06-04 01:06:28 -0500

    Minor changes - Update LinkedIn Link - Udpate resume

--- react-portfolio/src/components/Resume/Resume.jsx
+++ react-portfolio/src/components/Resume/Resume.jsx
@@ -6,7 +6,7 @@ export const Resume = () =>{
     return <section className={styles.container} id="resume">
         <h2 className={styles.title}>Resume</h2>
         <div>
-        <iframe src="https://drive.google.com/file/d/1fICdCtB0_QBUU7TowQOO0jh2wS7374XU/preview" 
+        <iframe src="https://drive.google.com/file/d/1QSXFCHVUgpobcOHjs8SI5e_dkdc0OUBj/preview" 
         width="640" height="480" allow="autoplay"></iframe>
 
         </div>
